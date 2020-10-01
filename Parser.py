375f6a992a550a66d0199e3dd3c40788c9b6a3bd
brauliovillalobos <baluca26@gmail.com>
2020-09-30 21:41:31 -0600

    Entrega final del Parser

--- Parser.py
+++ Parser.py
@@ -6,7 +6,7 @@
 # ------------------------------------------------------------
 import os
 with open("outfile.txt","r") as file: data=file.read().replace('\n', '')
-
+import pandas as pd
 # List of token names.   This is always required
 tokens = (
     'INDICADORT',
@@ -34,10 +34,8 @@ tokens = (
 t_INDICADORT = r'<COD_INDICADORINTERNO>'
 t_INDICADORTC = r'</COD_INDICADORINTERNO>'
 t_NUMT = r'<NUM_VALOR>'
-#t_NUM = r'\d{3}\.\d+'
 t_NUMTC = r'</NUM_VALOR>'
 t_FECHAT = r'<DES_FECHA>'
-#t_FECHA = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}'
 t_FECHATC = r'</DES_FECHA>'
 t_DIFFGR = r'<diffgr.+-diffgram-v\d">'
 t_DIFFGRC = r'</diffgr:diffgram>'
@@ -49,8 +47,6 @@ t_DATASET = r'DataSet\sxmlns="http://ws.sdde.bccr.fi.cr">'
 t_DATASETC = r'</DataSet>'
 t_LETTERS = r'[a-zA-Z]+'
 t_TRASH = r'<xs:([a-zA-Z]+|\s+|[0-9]+|(=|\"|_|:|/|.|-)+)>{1}'
-
-#<xs:schema id="Datos_de_INGC011_CAT_INDICADORECONOMIC" xmlns="" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
 
 #Regla de Fecha
 
@@ -98,53 +94,32 @@ names = { }
 
 def p_statement_DF(t): #Paso 1
     'statement : DIFFGR CDF DIFFGRC'
-    print(t[1])
-    print(t[3])
 
 def p_statement_CDF(t): #Paso 2
     'CDF : DATOSINICT CCDFS DATOSINICTC'
-    print(t[1])
-    print(t[3])
 
 def p_statement_CCDFS(t):
     'CCDFS : CCDF CCDFS'
-    print(t[1])
-    print(t[2])
 
 def p_statement_CCDFSBase(t):
     'CCDFS : CCDF'
-    print(t[1])
+
 def p_statement_CCDF(t): #REGLA 1 de CCDF
     'CCDF : ING CING INGC'
-    print(t[1])
-    print(t[2])
-    print(t[3])
-
-
-#<diffgr:diffgram xmlns:msdata="urn:schemas-microsoft-com:xml-msdata" xmlns:diffgr="urn:schemas-microsoft-com:xml-diffgram-v1"> <Datos_de_INGC011_CAT_INDICADORECONOMIC xmlns=""> <INGC011_CAT_INDICADORECONOMIC diffgr:id="INGC011_CAT_INDICADORECONOMIC1" msdata:rowOrder="0"> <DES_FECHA>2019-05-25T00:00:00-06:00</DES_FECHA> <NUM_VALOR>588.41000000</NUM_VALOR> </INGC011_CAT_INDICADORECONOMIC> <INGC011_CAT_INDICADORECONOMIC diffgr:id="INGC011_CAT_INDICADORECONOMIC2" msdata:rowOrder="1"> <DES_FECHA>2019-05-26T00:00:00-06:00</DES_FECHA> <NUM_VALOR>588.41000000</NUM_VALOR> </INGC011_CAT_INDICADORECONOMIC> <INGC011_CAT_INDICADORECONOMIC diffgr:id="INGC011_CAT_INDICADORECONOMIC3" msdata:rowOrder="2"> <DES_FECHA>2019-05-27T00:00:00-06:00</DES_FECHA> <NUM_VALOR>588.41000000</NUM_VALOR> </INGC011_CAT_INDICADORECONOMIC> </Datos_de_INGC011_CAT_INDICADORECONOMIC> </diffgr:diffgram>
 
 def p_statement_CING(t):
     'CING : INDICADOR DATE VALOR'
 
 def p_statement_INDICADOR(t):
     'INDICADOR : INDICADORT NUMBER INDICADORTC'
-    print(t[1])
-    print(t[2])
-    print(t[3])
     indicador.append(t[2])
 
 def p_statement_DATE(t):
     'DATE : FECHAT FECHA FECHATC'
-    print(t[1])
-    print(t[2])
-    print(t[3])
     dates.append(t[2])
 
 def p_statement_VALOR(t):
     'VALOR : NUMT NUM NUMTC'
-    print(t[1])
-    print(t[2])
-    print(t[3])
     values.append(t[2])
 
 def p_error(t):
@@ -154,10 +129,8 @@ import ply.yacc as yacc
 parser = yacc.yacc()
 parser.parse(data)
 
-print(dates)
-print(values)
-print(indicador)
-
+df = pd.DataFrame(list(zip(indicador,dates,values)), columns=['Indicador','Fecha','Tipo Cambio'])
+print(df)
 #while True:
 #    try:
 #        s = input('Input')   # Use raw_input on Python 2
